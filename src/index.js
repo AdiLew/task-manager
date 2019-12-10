dc96ff7df363d1fafef8e33072654cf8289ef5a8
AdiLew <adilu.lev@gmail.com>
2019-12-10 22:10:31 +0200

    remove middleware examples comments

--- src/index.js
+++ src/index.js
@@ -6,39 +6,12 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next)=>{
-//     if (req.method === 'GET'){
-//         res.send('GET Requests are disabled')
-//     }
-//     else {
-//         next();
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Go away, we are busy!')
-// })
-
-
 app.use(express.json())
 
 //Routers
 app.use(userRouter)
 app.use(taskRouter);
 
-
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-// const main = async()=>{
-//     // const task = await Task.findById ('5dee23c70c4f522434a68979')
-//     // await task.populate('owner').execPopulate();
-//     // console.log(task.owner)
-//     const user = await User.findById('5dee224144483c2f94697636')
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks);
-// }
-// main()
