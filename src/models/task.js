de264a86b74037a6da76d8dd8c860bfd600b0c06
AdiLew <adilu.lev@gmail.com>
2020-11-07 20:52:43 +0200

    Align to lesson 140

--- src/models/task.js
+++ src/models/task.js
@@ -15,9 +15,10 @@ const taskSchema = new mongoose.Schema({
         required: true,
         ref: 'User'
     }
-},{
+}, {
     timestamps: true
 })
-const Task = mongoose.model('Task', taskSchema )
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
